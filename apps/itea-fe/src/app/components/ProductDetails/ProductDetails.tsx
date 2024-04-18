d0eaad37f05d45c6843a6bf0fa69cd11a5ddfb8e
MFrenken <mfrenken@it-economics.de>
2024-04-18 11:01:28 +0200

    added mock data for product details

--- apps/itea-fe/src/app/components/ProductDetails/ProductDetails.tsx
+++ apps/itea-fe/src/app/components/ProductDetails/ProductDetails.tsx
@@ -1,9 +1,35 @@
-import { FC } from 'react';
+import {FC, useEffect, useState} from 'react';
+import {Card, CardContent, CardHeader, CardMedia} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import {Product} from "../../model/Product";
 
-export const ProductDetails: FC = () => {
+export const ProductDetails: FC = ({Product}) => {
+
+  const [product, setProduct] = useState<Product>(
+    {id: '1', name: 'Product 1', description: 'Description 1', price: 100, imageUrl: 'https://via.placeholder.com/150'}
+  )
+
+  useEffect(() => {
+    //fetchData()
+  }, [])
+
   return (
     <div>
-      <h1>Welcome to ProductDetails!</h1>
+      <Card>
+        <CardHeader
+          title={product.name}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          src={product.imageUrl}
+          alt="Concert"
+        />
+        <CardContent>
+          <Typography>{product.description}</Typography>
+          <Typography><strong>Price: {product.price}</strong></Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 };
