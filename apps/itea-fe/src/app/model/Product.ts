81dcc606a5932302d2a19693366819cf22d3062e
nboennighausen <nils.boennighausen@it-economics.com>
2024-04-18 16:36:11 +0200

    start adding product hooks (no fetch but generated classes, that don't exist yet)

--- apps/itea-fe/src/app/model/Product.ts
+++ apps/itea-fe/src/app/model/Product.ts
@@ -5,3 +5,13 @@ export interface Product {
   price: number;
   imageUrl: string;
 }
+
+export const convertFromApiProduct = (apiProduct: any): Product => { //todo add type when api was generated
+  return {
+    id: apiProduct.id,
+    name: apiProduct.name,
+    description: apiProduct.description,
+    price: apiProduct.price,
+    imageUrl: "" //todo get image url either "hardcoded" or add to backend
+  }
+}
---
+++ apps/itea-fe/src/app/model/hooks/ProductsApiClient.ts
@@ -0,0 +1,1 @@
+export const productsApiClient = new ProductsApi(new Configuration({ basePath: "products" })) //todo import products api and configuration from generated api
