72bd480d4e2379b55e2042b31b48d3d533f1ee16
Vladimir Daskalov <vdaskalov@it-economics.de>
2024-06-12 13:58:04 +0200

    refactor: product tile to use link

--- apps/itea-fe/src/app/components/Gallery/ProductTile.tsx
+++ apps/itea-fe/src/app/components/Gallery/ProductTile.tsx
@@ -1,32 +1,34 @@
-import {FC} from 'react';
-import {Card, CardContent, CardHeader, CardMedia} from "@mui/material";
-import {useNavigate} from "react-router-dom";
-import {Product} from "../../model/Product/Product";
-import Typography from "@mui/material/Typography";
+import { Card, CardContent, CardHeader, CardMedia } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { FC } from 'react';
+import { Product } from '../../model/Product/Product';
+import { Link } from 'react-router-dom';
 
 interface ProductTileProps {
   product: Product;
 }
 
-export const ProductTile: FC<ProductTileProps> = ({product}) => {
-
-  const navigate = useNavigate()
-
+export const ProductTile: FC<ProductTileProps> = ({ product }) => {
   return (
-    <Card onClick={() => navigate(`/product/details/${product.id}`)}>
-      <CardHeader
-        title={product.name}
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        src={product.imageName} //todo use it to get image
-        alt="Concert"
-      />
-      <CardContent>
-      <Typography>{product.description}</Typography>
-      <Typography><strong>Price: {product.price}</strong></Typography>
-      </CardContent>
-    </Card>
+    <Link
+      to={`/product/details/${product.id}`}
+      style={{ textDecoration: 'none' }}
+    >
+      <Card>
+        <CardHeader title={product.name} />
+        <CardMedia
+          component="img"
+          height="194"
+          src={product.imageName} //todo use it to get image
+          alt="Concert"
+        />
+        <CardContent>
+          <Typography>{product.description}</Typography>
+          <Typography>
+            <strong>Price: {product.price}</strong>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
