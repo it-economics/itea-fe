b34c2666a11ca41fb424f6f70125804df7ed8dd4
Vladimir Daskalov <vdaskalov@it-economics.de>
2024-06-13 10:44:46 +0200

    refactor: mocks

--- apps/itea-fe/src/mocks/handlers.ts
+++ apps/itea-fe/src/mocks/handlers.ts
@@ -1,120 +1,28 @@
-import {http, HttpResponse} from 'msw';
+import { http, HttpResponse } from 'msw';
+import { products } from './products';
 
-export const checkoutEndpoint = "/checkout" //todo no longer export when only used here
-export const productEndpoint = "/product/details" //todo check if correct and put into endpoint file
-export const allProductsEndpoint = "/products" //todo check if correct and put into endpoint file
-export const usersEndpoint = "user" //todo check if correct and put into endpoint file
-
+export const checkoutEndpoint = '/checkout'; //todo no longer export when only used here
+export const productEndpoint = '/product/details'; //todo check if correct and put into endpoint file
+export const allProductsEndpoint = '/products'; //todo check if correct and put into endpoint file
+export const usersEndpoint = 'user'; //todo check if correct and put into endpoint file
 
 export const handlers = [
   http.get(allProductsEndpoint, () => {
     return HttpResponse.json({
-      products: [
-        {
-          id: '1',
-          name: 'Lamp',
-          description: 'It really brightens the room',
-          price: 30,
-          imageName: '../assets/images/pexels-colour-creation-28649-112811.jpg'
-        },
-        {
-          id: '2',
-          name: 'Table',
-          description: 'Better than nothing',
-          price: 250,
-          imageName: '../assets/images/pexels-blankspace-2647714.jpg'
-        },
-        {
-          id: '3',
-          name: 'Clothing Rack',
-          description: 'It holds your stuff',
-          price: 100,
-          imageName: '../assets/images/pexels-liza-summer-6347597.jpg'
-        },
-        {
-          id: '4',
-          name: 'Couch',
-          description: 'Nice and comfy',
-          price: 350,
-          imageName: '../assets/images/pexels-martinpechy-1866149.jpg'
-        },
-        {
-          id: '5',
-          name: 'Chair',
-          description: 'Nice and not so comfy',
-          price: 150,
-          imageName: '../assets/images/pexels-paula-schmidt-353488-963486.jpg'
-        },
-      ],
+      products,
     });
   }),
 
-  http.get(productEndpoint + '/1', () => {
-    return HttpResponse.json({
-      product:
-        {
-          id: '1',
-          name: 'Lamp',
-          description: 'It really brightens the room',
-          price: 30,
-          imageName: '../assets/images/pexels-colour-creation-28649-112811.jpg'
-        },
-    });
+  http.get<{ id: string }>(`${productEndpoint}/:id`, ({ params }) => {
+    const productId = params.id;
+    const product = products.find((product) => product.id === productId);
+
+    return product
+      ? HttpResponse.json({
+          product,
+        })
+      : HttpResponse.json({ error: 'Product not found' }, { status: 404 });
   }),
-
-  http.get(productEndpoint + '/2', () => {
-    return HttpResponse.json({
-      product:
-        {
-          id: '2',
-          name: 'Table',
-          description: 'Better than nothing',
-          price: 250,
-          imageName: '../assets/images/pexels-blankspace-2647714.jpg'
-        },
-    });
-  }),
-
-  http.get(productEndpoint + '/3', () => {
-    return HttpResponse.json({
-      product:
-        {
-          id: '3',
-          name: 'Clothing Rack',
-          description: 'It holds your stuff',
-          price: 100,
-          imageName: '../assets/images/pexels-liza-summer-6347597.jpg'
-        },
-    });
-  }),
-
-  http.get(productEndpoint + '/4', () => {
-    return HttpResponse.json({
-      product:
-        {
-          id: '4',
-          name: 'Couch',
-          description: 'Nice and comfy',
-          price: 350,
-          imageName: '../assets/images/pexels-martinpechy-1866149.jpg'
-        },
-    });
-  }),
-
-  http.get(productEndpoint + '/5', () => {
-    return HttpResponse.json({
-      product:
-        {
-          id: '5',
-          name: 'Chair',
-          description: 'Nice and not so comfy',
-          price: 150,
-          imageName: '../assets/images/pexels-paula-schmidt-353488-963486.jpg'
-        },
-    });
-  }),
-
-
 
   //todo add body
   http.post(checkoutEndpoint, () => {
---
+++ apps/itea-fe/src/mocks/products.ts
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: '424bc972-5a79-4b82-b5d9-f32dfad458fc',
+    name: 'Lamp',
+    description: 'It really brightens the room',
+    price: 30,
+    imageName: '../assets/images/pexels-colour-creation-28649-112811.jpg',
+  },
+  {
+    id: '2ce1fb4e-85a3-4900-87d1-bcf38ac736fd',
+    name: 'Table',
+    description: 'Better than nothing',
+    price: 250,
+    imageName: '../assets/images/pexels-blankspace-2647714.jpg',
+  },
+  {
+    id: 'b5781224-25d0-4b52-9555-48abe2b3299c',
+    name: 'Clothing Rack',
+    description: 'It holds your stuff',
+    price: 100,
+    imageName: '../assets/images/pexels-liza-summer-6347597.jpg',
+  },
+  {
+    id: '8a7a3802-485b-4f25-9f6d-f47238e0d2f4',
+    name: 'Couch',
+    description: 'Nice and comfy',
+    price: 350,
+    imageName: '../assets/images/pexels-martinpechy-1866149.jpg',
+  },
+  {
+    id: 'f5178be4-32b7-493a-b60f-b53e4e9b87ca',
+    name: 'Chair',
+    description: 'Nice and not so comfy',
+    price: 150,
+    imageName: '../assets/images/pexels-paula-schmidt-353488-963486.jpg',
+  },
+];
