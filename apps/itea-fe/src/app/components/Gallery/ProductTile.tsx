2a89934b6a3f1de8efa4ac803a2555a53d4576bc
Vladimir Daskalov <vdaskalov@it-economics.de>
2024-06-13 10:29:43 +0200

    refactor: product tile and product details

--- apps/itea-fe/src/app/components/Gallery/ProductTile.tsx
+++ apps/itea-fe/src/app/components/Gallery/ProductTile.tsx
@@ -1,9 +1,8 @@
-import {Card, CardContent} from '@mui/material';
-import Typography from '@mui/material/Typography';
-import {FC} from 'react';
-import {Product} from '../../model/Product/Product';
-import {Link} from 'react-router-dom';
-import {AspectRatio} from "@mui/icons-material";
+import { Card, CardActions, CardContent, Typography } from '@mui/material';
+import { FC } from 'react';
+import { Product } from '../../model/Product/Product';
+import { Link } from 'react-router-dom';
+import { AspectRatio } from '@mui/icons-material';
 
 interface ProductTileProps {
   product: Product;
@@ -15,25 +14,22 @@ export const ProductTile: FC<ProductTileProps> = ({ product }) => {
       to={`/product/details/${product.id}`}
       style={{ textDecoration: 'none' }}
     >
-      <Card sx={{ width: 320 }}>
-        <div>
-          <Typography level="title-lg">{product.name}</Typography>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
+      <Card sx={(theme) => ({ width: 320, padding: theme.spacing() })}>
+        <Typography>{product.name}</Typography>
+
+        <AspectRatio>
           <img
             src="../../../assets/images/pexels-colour-creation-28649-112811.jpg"
             loading="lazy"
             alt="../../../assets/images/pexels-colour-creation-28649-112811.jpg"
           />
         </AspectRatio>
-        <CardContent orientation="horizontal">
-          <div>
-            <Typography level="body-xs">{product.description}</Typography>
-            <Typography level="body-xs">Price:</Typography>
-            <Typography fontSize="lg" fontWeight="lg">
-              {product.price}
-            </Typography>
-          </div>
+        <CardContent>
+          <Typography>{product.description}</Typography>
+          <Typography>Price:</Typography>
+          <Typography fontSize="lg" fontWeight="lg">
+            {product.price}
+          </Typography>
         </CardContent>
       </Card>
     </Link>
--- apps/itea-fe/src/app/components/ProductDetails/ProductDetails.tsx
+++ apps/itea-fe/src/app/components/ProductDetails/ProductDetails.tsx
@@ -1,65 +1,74 @@
-import {FC, useEffect, useState} from 'react';
-import {Button, Card, CardContent, Grid} from "@mui/material";
-import Typography from "@mui/material/Typography";
-import {Product} from "../../model/Product/Product";
-import {AspectRatio} from "@mui/icons-material";
-import {useStoreProducts} from "../../contexts/StoreProductsContext";
-import {useGetProduct} from "../../model/Product/hooks/useGetProducts";
-import {useParams} from "react-router-dom";
+import { AspectRatio } from '@mui/icons-material';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CircularProgress,
+} from '@mui/material';
+import Typography from '@mui/material/Typography';
+import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { useStoreProducts } from '../../contexts/StoreProductsContext';
+import { useGetProduct } from '../../model/Product/hooks/useGetProducts';
 
 export const ProductDetails: FC = () => {
-  const {id} = useParams<{id: string}>()
+  const { id } = useParams<{ id: string }>();
   const { add2Cart } = useStoreProducts();
 
-  const [product, setProduct] = useState<Product | undefined>()
-
-  const {data} = useGetProduct(id)
-
-  useEffect(() => {
-    setProduct(data)
-  }, [data])
+  const { data: product, isLoading } = useGetProduct(id || '');
 
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justify="center"
-      style={{minHeight: '100vh'}}
+    <Box
+      style={{
+        height: '100%',
+        width: '100%',
+        alignItems: 'center',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
     >
-      <Card sx={{ width: 320 }}>
-        <div>
-          <Typography level="title-lg">{product?.name}</Typography>
-        </div>
-        <AspectRatio minHeight="120px" maxHeight="200px">
-          <img
-            src={product?.imageName ?? ''}
-            srcSet={product?.imageName ?? ''}
-            loading="lazy"
-            alt=""
-          />
-        </AspectRatio>
-        <CardContent orientation="horizontal">
-          <div>
-            <Typography level="body-xs">{product?.description}</Typography>
-            <Typography level="body-xs">Price:</Typography>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <Card sx={(theme) => ({ width: 320, padding: theme.spacing() })}>
+          <Typography>{product?.name}</Typography>
+          <AspectRatio>
+            <img
+              src={product?.imageName ?? ''}
+              srcSet={product?.imageName ?? ''}
+              loading="lazy"
+              alt=""
+            />
+          </AspectRatio>
+          <CardContent>
+            <Typography>{product?.description}</Typography>
+            <Typography>Price:</Typography>
             <Typography fontSize="lg" fontWeight="lg">
               {product?.price}
             </Typography>
-          </div>
-          <Button
-            variant="solid"
-            size="md"
-            color="primary"
-            aria-label="Add to Cart"
-            sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
-            onClick={() => add2Cart(product)}
-          >
-            Add to Cart
-          </Button>
-        </CardContent>
-      </Card>
-    </Grid>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              size="medium"
+              color="primary"
+              startIcon={<AddShoppingCartIcon />}
+              aria-label="Add to Cart"
+              sx={{ ml: 'auto', alignSelf: 'center', fontWeight: 600 }}
+              onClick={() => {
+                if (product) {
+                  add2Cart(product);
+                }
+              }}
+            >
+              Add to Cart
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </Box>
   );
 };
